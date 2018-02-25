6cd85947079f7a93e4f1600070ef044414c062a6
gmarilia <g_marilia@hotmail.com>
2018-02-25 17:13:12 -0300

    Add files via upload

---
+++ pages/monitoramento/menu.php
@@ -0,0 +1,27 @@
+<div class="row">
+    <div class="col-md-12"><h3 class="text-center" style="color:green">Plataforma de Gerenciamento de midias Sociais</h3></div>
+</div>
+<div class="row"> 
+    <div class="col-md-12">                                  
+        <ul class="nav nav-pills">
+            <li role="presentation">
+                <a href=".">Monitoramento</a>
+            </li>
+            <li role="presentation">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#" 
+                 role="button" aria-haspopup="true" aria-expanded="false">
+                    Analítico <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li role="presentation">
+                        <a href=".?page=analitico">Graficos</a>
+                    </li>                                    
+                </ul>                      
+
+            </li>
+            <li role="presentation">
+                <a href="?page=reclamacoes">Reclamações</a>
+            </li>
+        </ul>
+    </div>
+</div>
